fe89de48c39829f7fd594313e139aca6fff6d06f
Брянцев Александр <bryancev.a2@wildberries.work>
2024-06-21 21:18:34 +0300

    feat: add swagger

--- myblog/events/urls.py
+++ myblog/events/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 from . import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', views.EventView.as_view()),
@@ -7,5 +23,7 @@ urlpatterns = [
     path('review/<int:pk>/', views.AddComments.as_view(), name='event_add_comments'),
     path('<int:pk>/add_likes/', views.AddLike.as_view(), name='event_add_likes'),
     path('<int:pk>/del_likes/', views.DelLike.as_view(), name='event_del_likes'),
-    path('api/eventlist', views.EventListCreateAPIView.as_view())
+    path('api/eventlist', views.EventListCreateAPIView.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- myblog/news/urls.py
+++ myblog/news/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from . import views
 from .viewsets import PostListViewSet
@@ -7,17 +10,29 @@ from .viewsets import PostListViewSet
 router = DefaultRouter()
 router.register(r'posts', PostListViewSet, basename='post')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('', views.PostView.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('review/<int:pk>/', views.AddComments.as_view(), name='add_comments'),
     path('<int:pk>/add_likes/', views.AddLike.as_view(), name='add_likes'),
     path('<int:pk>/del_likes/', views.DelLike.as_view(), name='del_likes'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('register/', views.RegisterView.as_view(), name='register'),
     path('api/posts', views.PostListCreateAPIView.as_view()),
     path('api/posts/<int:pk>/add_likes/', views.LikeApiAdd.as_view()),
     path('api/posts/<int:pk>/del_likes/', views.LikeApiDel.as_view()),
     path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
