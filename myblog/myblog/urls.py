b65f7fd710b7212ddb3da192394c093a4581c0db
Брянцев Александр <bryancev.a2@wildberries.work>
2024-06-21 21:26:06 +0300

    feat: redirect to news

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from myblog.views import redirect_to_news
+
 urlpatterns = [
+    path('', redirect_to_news),
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
     path('events/', include('events.urls')),
