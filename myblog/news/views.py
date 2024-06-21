03bf067576694421f1cda8887d6433e516b2f968
Брянцев Александр <bryancev.a2@wildberries.work>
2024-06-21 21:19:31 +0300

    feat: authorisation

--- myblog/news/views.py
+++ myblog/news/views.py
@@ -8,30 +8,15 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .models import Post, Likes, Comments
 from .form import CommentsForm
-
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-from django.contrib.auth import authenticate, login, logout
 from django.views import View
-
-
-
 from django.views.generic.base import TemplateView
-
 from .serializers import PostSerializer
 
 
 class PostView(TemplateView):
     template_name = 'news/news.html'  # Шаблон, который будет использоваться для отображения контента
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:  # Проверка аутентификации пользователя
-            return redirect('login')  # Перенаправление на страницу авторизации
-        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -92,7 +77,6 @@ class AddLike(View):
             new_like.save()
             return redirect(f'/{pk}')
 
-
 class DelLike(View):
     def get(self, request, pk):
         ip_client = get_client_ip(request)
@@ -102,37 +86,6 @@ class DelLike(View):
             return redirect(f'/{pk}')
         except:
             return redirect(f'/{pk}')
-
-
-class LoginView(View):
-    def get(self, request):
-        return render(request, 'registration/login.html')
-
-    def post(self, request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
-
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
-        return redirect('/')
-
-class RegisterView(View):
-    def get(self, request):
-        form = UserCreationForm()
-        return render(request, 'registration/registration.html', {'form': form})
-
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-        return render(request, 'registration/registration.html', {'form': form})
 
 class PostApiViewPagination(PageNumberPagination):
     page_size = 5;
